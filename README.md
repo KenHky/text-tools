988f0418990e9fcfd7c57fa352effcf4b1e0ef01
黄凯羿 <huangkaiyi@huangkaiyideMacBook-Pro-2.local>
2021-09-08 17:05:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# text-tools README
+
+This is a tool library for solving strange text problems
+
+## Features
+
+1. constant in object:
+```
+shortcut key: cmd + alt + u(mac) ctrl + alt + u(win);
+```
+
+![package gif](./demo.gif)
+
+
---
+++ src/extension.ts
@@ -0,0 +1,57 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+    const getStringArray = function (text: string) {
+        const str = [];
+        let lastPos = 0,
+            i;
+
+        for (i = 0; i < text.length; i++) {
+            if (/[\s\-:,_A-Z]/.test(text.charAt(i))) {
+                const s = text.substring(lastPos, i).replace(/[\s\-:,_]/, "");
+                if (s.length > 0) {
+                    str.push(s.toLowerCase());
+                }
+                lastPos = i;
+            }
+        }
+
+        if (lastPos < text.length) {
+            const s = text.substring(lastPos, i).replace(/[\s\-:,_]/, "");
+            if (s.length > 0) {
+                str.push(s.toLowerCase());
+            }
+        }
+
+        return str;
+    };
+    const constantInObject = vscode.commands.registerCommand("extension.constantInObject", function () {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            if (!editor.selections || editor.selections.length === 0) {
+                vscode.window.showInformationMessage("There is no selected text!");
+                return;
+            }
+
+            editor.edit(function (edit) {
+                for (let i = 0; i < editor.selections.length; i++) {
+                    const selection = editor.selections[i];
+
+                    if (!selection.isEmpty) {
+                        const range = new vscode.Range(selection.start, selection.end);
+                        const defaultText = editor.document.getText(range);
+                        let text = defaultText;
+                        text = getStringArray(text).join('_');
+                        text = text.toUpperCase() + ': "' + defaultText + '",';
+                        edit.replace(range, text);
+                    }
+                }
+            });
+        } else {
+            vscode.window.showInformationMessage("There is no activeTextEditor!");
+        }
+    });
+    context.subscriptions.push(constantInObject);
+}
+
+export function deactivate() {}
